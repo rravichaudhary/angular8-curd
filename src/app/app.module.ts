cdbe05cb08faf3556c1ec4e6a484077fd76b3197
Ravi Chaudhary <ravic@beckett.com>
2019-11-29 19:25:14 +0530

    added all modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,95 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+
+import {
+  MatInputModule,
+  MatPaginatorModule,
+  MatProgressSpinnerModule,
+  MatSortModule,
+  MatTableModule,
+  MatIconModule,
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule } from "@angular/material";
+
+  import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductsComponent } from './products/products.component';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
+import { ProductAddComponent } from './products/product-add/product-add.component';
+import { ProductEditComponent } from './products/product-edit/product-edit.component';
+import { LoginComponent } from './login/login.component';
+import { ListUserComponent } from './user/list-user/list-user.component';
+import { AddUserComponent } from './user/add-user/add-user.component';
+import { EditUserComponent } from './user/edit-user/edit-user.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'products',
+    component: ProductsComponent,
+    data: { title: 'List of Products' }
+  },
+  {
+    path: 'product-details/:id',
+    component: ProductDetailComponent,
+    data: { title: 'Product Details' }
+  },
+  {
+    path: 'product-add',
+    component: ProductAddComponent,
+    data: { title: 'Add Product' }
+  },
+  {
+    path: 'product-edit/:id',
+    component: ProductEditComponent,
+    data: { title: 'Edit Product' }
+  },
+  { path: '',
+    redirectTo: '/products',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ProductDetailComponent,
+    ProductAddComponent,
+    ProductEditComponent,
+    LoginComponent,
+    ListUserComponent,
+    AddUserComponent,
+    EditUserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserModule,
+  FormsModule,
+  HttpClientModule,
+  ReactiveFormsModule,
+  BrowserAnimationsModule,
+  MatInputModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatProgressSpinnerModule,
+  MatIconModule,
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
